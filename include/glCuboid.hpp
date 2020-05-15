1efe8e5209764d7542fb342e82881ea5d21e039c
thewoz <leonardo.parisi@gmail.com>
2020-05-15 18:59:14 +0200

    Create glCuboid.hpp

---
+++ include/glCuboid.hpp
@@ -0,0 +1,191 @@
+/*
+ * MIT License
+ *
+ * Copyright © 2017
+ * Created by Leonardo Parisi (leonardo.parisi[at]gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef _H_MPL_OPENGL_CUBOID_H_
+#define _H_MPL_OPENGL_CUBOID_H_
+
+#include <cstdio>
+#include <cstdlib>
+
+#include "glObject.hpp"
+
+/*****************************************************************************/
+// namespace ogl
+/*****************************************************************************/
+namespace ogl {
+  
+  /*****************************************************************************/
+  // Class glCuboid
+  /*****************************************************************************/
+  class glCuboid : public glObject {
+    
+  private:
+    
+    GLuint vao = -1;
+    GLuint vbo[2];
+
+  public:
+        
+    /*****************************************************************************/
+    // glCuboid
+    /*****************************************************************************/
+    glCuboid(const std::string & _name = "") : glObject(_name) { }
+    
+    /*****************************************************************************/
+    // glCube
+    /*****************************************************************************/
+    glCuboid(const glm::vec3 & scale, int _style = glObject::STYLE::WIREFRAME, const glm::vec3 & _color = glm::vec3(0.0), const std::string & _name = "") : glObject(_name) { init(scale, _style, _color); }
+    
+    /*****************************************************************************/
+    // ~glCube
+    /*****************************************************************************/
+    ~glCuboid() { cleanInGpu(); }
+    
+    /*****************************************************************************/
+    // init
+    /*****************************************************************************/
+    void init(const glm::vec3 & scale, int _style = glObject::STYLE::WIREFRAME, const glm::vec3 & _color = glm::vec3(0.0)) {
+
+      DEBUG_LOG("glCube::init(" + name + ")");
+
+      glObject::initPlain(glm::vec3(0.0), glm::vec3(0.0), scale);
+
+      style = _style;
+      
+      color = _color;
+      
+      isInited = true;
+      
+    }
+    
+    /*****************************************************************************/
+    // render
+    /*****************************************************************************/
+    void render(const glm::mat4 & projection, const glm::mat4 & view) {
+            
+      DEBUG_LOG("glCube::render(" + name + ")");
+
+      glObject::renderBegin(projection, view);
+      
+      glDisable(GL_CULL_FACE); //NOTE: non sono sicuro che serva
+
+      glBindVertexArray(vao);
+      
+      //glEnableVertexAttribArray(0);
+      if(style == glObject::STYLE::WIREFRAME) glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+      if(style == glObject::STYLE::SOLID)     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+      
+      //glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, nullptr);
+      glDrawElements(GL_LINES, 24, GL_UNSIGNED_SHORT, nullptr);
+
+      
+      glBindVertexArray(0);
+      
+      glObject::renderEnd();
+
+    }
+    
+    private:
+
+    /*****************************************************************************/
+    // setInGpu
+    /*****************************************************************************/
+    void setInGpu() {
+      
+      DEBUG_LOG("glCube::setInGpu(" + name + ")");
+      
+      static const GLfloat vertices[] = {
+        // front
+        -1.0,  1.0,  1.0,
+         1.0,  1.0,  1.0,
+         1.0, -1.0,  1.0,
+        -1.0, -1.0,  1.0,
+        // back
+        -1.0,  1.0,  -1.0,
+        1.0,  1.0,  -1.0,
+        1.0, -1.0,  -1.0,
+        -1.0, -1.0, -1.0,
+      };
+      
+      static const GLushort indicies[] = {
+        // front
+        0, 1,
+        1, 2,
+        2, 3,
+        3, 0,
+        0, 4,
+        7, 4,
+        4, 5,
+        5, 6,
+        6, 7,
+        1, 5,
+        2, 6,
+        3, 7
+      };
+      
+      glGenVertexArrays(1, &vao);
+      glBindVertexArray(vao);
+      
+      glGenBuffers(2, vbo);
+
+      glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+      glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+      glEnableVertexAttribArray(0);
+      
+      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[1]);
+      glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+
+      glBindVertexArray(0);
+      glBindBuffer(GL_ARRAY_BUFFER, 0);
+      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+      
+    }
+    
+  private:
+    
+    /* ****************************************************************************/
+    // cleanInGpu() -
+    /* ****************************************************************************/
+    void cleanInGpu() {
+      
+      if(isInitedInGpu) {
+        
+        glDeleteBuffers(2, vbo);
+        glDeleteVertexArrays(1, &vao);
+        
+        isInitedInGpu = false;
+
+      }
+      
+    }
+    
+  };
+  
+} /* namespace ogl */
+
+#endif /* _H_MPL_OPENGL_CUBOID_H_ */
+
+
