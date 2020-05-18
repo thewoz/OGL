8f0956a3859d1e9439d9894ecd1601eff896f6fa
thewoz <leonardo.parisi@gmail.com>
2020-05-18 13:41:23 +0200

    imgui example

--- src/main.cpp
+++ src/main.cpp
@@ -82,8 +82,33 @@ int main(int argc, char * const argv []) {
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
     
-    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-    ImGui::ShowDemoWindow(&show_demo_window);
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color;
+    
+    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+    {
+      static float f = 0.0f;
+      static int counter = 0;
+      
+      ImGui::Begin("Params");                                 // Create a window called "Hello, world!" and append into it.
+      
+      ImGui::Text("List of Icaro parameters:");               // Display some text (you can use a format strings too)
+      ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+      ImGui::Checkbox("Another Window", &show_another_window);
+      
+      ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+      ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+      
+      if(ImGui::Button("Button"))                             // Buttons return true when clicked (most widgets return true when edited/activated)
+        counter++;
+      
+      ImGui::SameLine();
+      ImGui::Text("counter = %d", counter);
+      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+      
+      ImGui::End();
+    }
     
     // Rendering
     ImGui::Render();
