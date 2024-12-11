762bc738d837ab600a43854a56744d0643fed2f1
thewoz <leonardo.parisi@gmail.com>
2024-12-11 15:01:36 +0100

    Update install.sh

--- external/install.sh
+++ external/install.sh
@@ -6,28 +6,28 @@ if [ `uname` == "Darwin" ]; then
   brew install sdl2
   brew install assimp
   brew install glm
-  brew install premake
+  brew install premake4
 
 else
 
-  apt -y install libglfw3-dev
-  apt -y install libsdl2-dev
-  apt -y install libassimp-dev
-  apt -y install libglm-dev
-  apt -y install premake5
-  apt -y install libtiff-dev
-  apt -y install libfreetype-dev
+  sudo apt -y install libglfw3-dev
+  sudo apt -y install libsdl2-dev
+  sudo apt -y install libassimp-dev
+  sudo apt -y install libglm-dev
+  sudo apt -y install premake4
+  sudo apt -y install libtiff-dev
+  sudo apt -y install libfreetype-dev
+  
 fi
-
 
 # Controllo se la libreria SOIL2 è installata
 if [ ! -d "/usr/local/include/SOIL2/" ]; then
 
   git clone https://github.com/SpartanJ/SOIL2.git
   cd SOIL2
-  premake5 gmake
+  premake4 gmake
   cd make/*/
-  make soil2-static-lib config=release_x86_64
+  make config=release soil2-static-lib
   cd ../../lib/*
   sudo cp libsoil2.a /usr/local/lib
   cd ../../src/SOIL2/
