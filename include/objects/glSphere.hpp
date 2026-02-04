73b2242d60ae1bbd669924d599e975e6f8a594cb
thewoz <leonardo.parisi@gmail.com>
2026-02-04 15:53:47 +0100

    Fix line shader color for non-colored meshes

--- include/objects/glSphere.hpp
+++ include/objects/glSphere.hpp
@@ -73,7 +73,11 @@ namespace ogl {
 
         shader.setName(name);
         
-        shader.initPlain();
+        if(_style == glObject::STYLE::WIREFRAME) {
+          shader.initWireframe();
+        } else {
+          shader.initPlain();
+        }
         
         radius = _radius;
         
@@ -102,6 +106,18 @@ namespace ogl {
         
         if(isToInitInGpu()) initInGpu();
         
+        if(style == glObject::STYLE::WIREFRAME) {
+          if(shader.style != glShader::STYLE::WIREFRAME) {
+            shader.setName(name);
+            shader.initWireframe();
+          }
+        } else {
+          if(shader.style != glShader::STYLE::PLAIN) {
+            shader.setName(name);
+            shader.initPlain();
+          }
+        }
+
         shader.use();
 
         shader.setUniform("projection", camera->getProjection());
@@ -112,14 +128,14 @@ namespace ogl {
         glBindVertexArray(vao);
         
         if(style == glObject::STYLE::WIREFRAME) {
+          shader.setUniform("lineWidth", lineWidth);
+          shader.setUniform("viewport",  camera->getViewport());
           glDisable(GL_CULL_FACE);
-          glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         }
         
         if(style == glObject::STYLE::SOLID) {
           glEnable(GL_CULL_FACE);
           glCullFace(GL_BACK);
-          glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         }
         
         glDrawElements(GL_TRIANGLES, (slices * stacks + slices) * 6, GL_UNSIGNED_INT, nullptr);
